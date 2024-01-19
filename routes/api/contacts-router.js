4dac90b62e97e889fbeaa4bbe42d30168f0d3f32
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-19 12:47:42 +0200

    last check, everything is working

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,27 +2,45 @@ import express from "express";
 import contactsControllers from "../../controllers/contacts-controllers.js";
 import { isEmptyBody, isValidId } from "../../middlewares/index.js";
 import validateBody from "../../decorators/validateBody.js";
-import { contactAddSchema, contactUpdateFavoriteSchema, contactUpdateSchema } from "../../models/Contact.js";
+import {
+  contactAddSchema,
+  contactUpdateFavoriteSchema,
+  contactUpdateSchema,
+} from "../../models/Contact.js";
 
+const contactsRouter = express.Router();
 
-const contactsRouter = express.Router()
+contactsRouter.get("/", contactsControllers.getAll);
 
-contactsRouter.get('/', contactsControllers.getAll);
+contactsRouter.get("/:contactId", isValidId, contactsControllers.getById);
 
-contactsRouter.get('/:contactId', isValidId, contactsControllers.getById);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactAddSchema),
+  contactsControllers.addContact
+);
 
-contactsRouter.post('/', isEmptyBody, validateBody(contactAddSchema), contactsControllers.addContact);
+contactsRouter.delete(
+  "/:contactId",
+  isValidId,
+  contactsControllers.deleteContact
+);
 
-contactsRouter.delete('/:contactId', isValidId, contactsControllers.deleteContact);
-
-contactsRouter.put('/:contactId', isEmptyBody, validateBody(contactUpdateSchema), isValidId, contactsControllers.updateContact);
+contactsRouter.put(
+  "/:contactId",
+  isEmptyBody,
+  validateBody(contactUpdateSchema),
+  isValidId,
+  contactsControllers.updateContact
+);
 
 contactsRouter.patch(
-    "/:contactId/favorite",
-    isValidId,
-    isEmptyBody,
-    validateBody(contactUpdateFavoriteSchema),
-    contactsControllers.updateContact
-  );
+  "/:contactId/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactUpdateFavoriteSchema),
+  contactsControllers.updateContact
+);
 
 export default contactsRouter;
