a54f73cc13b1b98e602723f3e372c7cdcabc2492
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2024-01-19 09:20:36 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import { DB_HOST } from "./config.js";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful")
+    })
+  })
+  .catch (error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
 
